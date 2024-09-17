7fc3399ae64e190962999f358369934788262ff3
midCenturyDude7 <mgriffes@gmail.com>
2024-09-16 20:48:55 -0700

    Set up hamburger menu functionality with framer

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Navbar from "./components/Navbar";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col">
       <header>
-        <div className="text-2xl text-slate-300">Hello World!</div>
+        <Navbar />
       </header>
       <main className="flex flex-1 flex-col">
         <h1>Main Body Content</h1>
